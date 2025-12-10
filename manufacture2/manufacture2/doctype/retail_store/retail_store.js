b2c61f66d04aeef48bb3c73b890c35a41a0e0159
sappu-hub <sarfrasr@gmail.com>
2025-12-10 19:07:30 +0530

    feat:new doctype retail store

---
+++ manufacture2/manufacture2/doctype/retail_store/retail_store.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, man and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Retail Store", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ manufacture2/manufacture2/doctype/retail_store/test_retail_store.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, man and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRetailStore(FrappeTestCase):
+	pass
